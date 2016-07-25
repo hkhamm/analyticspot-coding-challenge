cc14935c77497bc505cd94b06004b3513e91f34e
Keith Hamm <keith.hamm@avantassessment.com>
2016-07-25 16:52:26 -0700

    Added README, started Main.java

--- src/main/java/com/hkhamm/Main.java
+++ src/main/java/com/hkhamm/Main.java
@@ -1,7 +1,22 @@
 package com.hkhamm;
 
-/**
- * Created by Keith on 7/25/16.
- */
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.Response;
+
+import java.util.concurrent.Future;
+
 public class Main {
+
+    private String url;
+
+    public Main(String url) {
+        this.url = url;
+    }
+
+    public void send(String message) {
+        AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+        // TODO add a body
+        Future<Response> future = asyncHttpClient.preparePost(url).addBodyPart();
+    }
 }
--- src/test/java/com/hkhamm/MainTest.java
+++ src/test/java/com/hkhamm/MainTest.java
@@ -1,7 +1,5 @@
 package com.hkhamm;
 
-/**
- * Created by Keith on 7/25/16.
- */
+
 public class MainTest {
 }
